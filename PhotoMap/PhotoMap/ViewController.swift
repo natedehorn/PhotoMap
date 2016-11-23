5c2fdb34d87dbf74388bc8f50cdebe4feaec8777
natedehorn <natedehorn@gmail.com>
2016-11-22 22:26:34 -0500

    Add upload photo and take photo functionality

--- PhotoMap/PhotoMap/ViewController.swift
+++ PhotoMap/PhotoMap/ViewController.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagepicker: UIImagePickerController!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagepicker = UIImagePickerController()
+            imagepicker.delegate = self
+            imagepicker.sourceType = .camera
+            self.present(imagepicker, animated: true, completion: nil)
+        }
+    }
+    @IBAction func photoLibrary(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagepicker = UIImagePickerController()
+            imagepicker.delegate = self
+            imagepicker.sourceType = .photoLibrary
+            self.present(imagepicker, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +37,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
 }
 

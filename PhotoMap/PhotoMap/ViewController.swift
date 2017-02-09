761a265f93340935f7246aa8647c61b28f38706f
Nate DeHorn <natedehorn@gmail.com>
2017-02-08 22:49:58 -0500

    Add pin to map at coordinates of image

--- PhotoMap/PhotoMap/ViewController.swift
+++ PhotoMap/PhotoMap/ViewController.swift
@@ -7,61 +7,61 @@
 //
 
 import UIKit
+import MapKit
 import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var imagepicker: UIImagePickerController!
+    var selectedCoords: CLLocationCoordinate2D!
     @IBOutlet weak var imageView: UIImageView!
-    
+    @IBOutlet var mapView: MKMapView!
+
     /**
-    Function called when "Take Photo" button is pressed
+        Function called when "View My Map" button is pressed
     */
-    /*@IBAction func takePhoto(_ sender: Any) {
-        imagepickersetup(source: .camera)
-    }*/
+    @IBAction func viewMyMap(_ sender: Any) {
+    }
     
     /**
-     Function called when "Photo Library" button is pressed
-     */
+        Function called when "Photo Library" button is pressed
+    */
     @IBAction func photoLibrary(_ sender: Any) {
         imagepickersetup(source:.photoLibrary)
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /**
+        Function called when "Add to Map" button is pressed
+    */
+    @IBAction func addToMap(_ sender: Any) {
+        if (imageView.image == nil) {
+            print("No image selected")
+            return
+        }
+        let annotation = MKPointAnnotation()
+        print(selectedCoords)
+        annotation.coordinate = selectedCoords
+        mapView.addAnnotation(annotation)
     }
     
     /**
-    Loads the photo selected into the app home screen
+        Loads the photo selected into the app home screen
     */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        print("image picker controller invoked")
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .scaleAspectFit
-            imageView.image = pickedImage
-        }
-        self.dismiss(animated: true, completion: nil)
-        if picker.sourceType == UIImagePickerControllerSourceType.photoLibrary {
+            imageView.image = image
             let url = info[UIImagePickerControllerReferenceURL] as! NSURL
             let fetchResult = PHAsset.fetchAssets(withALAssetURLs: [url as URL], options: nil)
             let asset = fetchResult.firstObject
-            print("Latitude: \(asset!.location!.coordinate.latitude)")
-            print("Longitude: \(asset!.location!.coordinate.longitude)")
+            selectedCoords = CLLocationCoordinate2D(
+                latitude: asset!.location!.coordinate.latitude,
+                longitude: asset!.location!.coordinate.longitude
+            )
         }
-        if picker.sourceType == UIImagePickerControllerSourceType.camera {
-            //When camera is used to upload photo
-        }
-        
-        
+        self.dismiss(animated: true, completion: nil)
     }
-    
     /**
-     Opens image source type
+        Opens image source type
     */
     func imagepickersetup(source:UIImagePickerControllerSourceType) {
         if UIImagePickerController.isSourceTypeAvailable(source) {
@@ -70,5 +70,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imagepicker.sourceType = source
             self.present(imagepicker, animated: true, completion: nil)
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 }
